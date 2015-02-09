fd0987a98874e3047693a3e08442952c89f2a8fa
nikwolfe7 <nikwolfe7@gmail.com>
2015-02-08 21:46:24 -0500

    Delayed request script was killing Polly... no thanks!

--- Check_Delayed_Requests.bat
+++ Check_Delayed_Requests.bat
@@ -1,12 +1,4 @@
 @echo off
-tasklist /nh /fi "imagename eq python.exe" | find /i "python.exe" >nul && (
-echo Python is running
-taskkill /f /im "python.exe"
 cd C:\Users\PD\Documents\polly\pythonDongle
 start python delayed_requests.py
-) || (
-echo Python is not running
-cd C:\Users\PD\Documents\polly\pythonDongle
-start python delayed_requests.py
-)
 :end
